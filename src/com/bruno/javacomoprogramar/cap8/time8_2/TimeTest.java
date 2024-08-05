decf5512db938551be9c4c7896ec30f03094443a
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-08-05 10:08:06 -0300

    Feat: cap8 - m8.2 - classe TimeTest

---
+++ src/com/bruno/javacomoprogramar/cap8/time8_2/TimeTest.java
@@ -0,0 +1,36 @@
+package com.bruno.javacomoprogramar.cap8.time8_2;
+
+public class TimeTest {
+    public static void main(String[] args) {
+
+        // criar um objeto da classe time
+        Time time = new Time();
+
+        // teste para verificar se o objeto foi corretamente chamado
+        DisplayTime("After time object is created", time);
+        System.out.println();
+
+        // inserir valores para hora, minuto e segundo
+        time.setTime(13, 27, 6);
+        DisplayTime("After calling setTime", time);
+        System.out.println();
+
+        // inserir valores não aceitos no método
+        try {
+            time.setTime(99, 99, 99);
+        }
+        catch (IllegalArgumentException ex)
+        {
+            System.out.printf("Exception: %s%n%n", ex.getMessage());
+        }
+
+        // verificar valores após chamada com argumentos ilegais
+        DisplayTime("Chamada após inserção de argumentos inválidos", time);
+
+    }
+
+    private static void DisplayTime(String header, Time time)
+    {
+        System.out.printf("%s%nUniversal time: %s%nStandard time: %s%n", header, time.toUniversalString(), time.toString());
+    }
+}
