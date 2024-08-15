0a974892f6f6957e0d76cf581dc2fb4d6044f1ea
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-08-15 10:35:06 -0300

    Cap8 - M88 composicao: classe Employee

---
+++ src/com/bruno/javacomoprogramar/cap8/composicao88/Employee.java
@@ -0,0 +1,24 @@
+package com.bruno.javacomoprogramar.cap8.composicao88;
+
+public class Employee {
+
+    // Atributos
+    private String firstName;
+    private String lastName;
+    private Date birthDate; // composição
+    private Date hireDate; // composição
+
+    // Construtor
+    public Employee(String firstName, String lastName, Date birthDate, Date hireDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.hireDate = hireDate;
+    }
+
+    public String toString()
+    {
+        return String.format("%s | %s | Hired: %s | Birthday: %s", lastName, firstName, hireDate, birthDate);
+    }
+
+}
